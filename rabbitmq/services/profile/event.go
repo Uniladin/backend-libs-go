8fbd31b601e974e1eb9849805dd2ad4d021a3eb9
Trung Nguyễn <quachquesh2@gmail.com>
2026-01-22 12:13:45 +0700

    refactor(rabbitmq): update name profile event

--- rabbitmq/services/profile/event.go
+++ rabbitmq/services/profile/event.go
@@ -1,16 +1,16 @@
 package profile
 
-type ProfileChangeEmailEvent struct {
+type EventProfileChangeEmail struct {
 	UserID   string `json:"user_id"`
 	OldEmail string `json:"old_email"`
 	NewEmail string `json:"new_email"`
 }
 
-type ProfileDeletedEvent struct {
+type EventProfileDeleted struct {
 	UserID    string `json:"user_id"`
 	DeletedAt int64  `json:"deleted_at"`
 }
 
-type ProfileUnDeletedEvent struct {
+type EventProfileUnDeleted struct {
 	UserID string `json:"user_id"`
 }
